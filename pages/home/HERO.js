a25492cd0257eb1c209e85d0b0115de924f97843
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-06-16 17:33:11 +0530

    airbnb

--- pages/home/HERO.js
+++ pages/home/HERO.js
@@ -46,7 +46,7 @@ export default function HERO() {
         <SwiperSlide>
           <div key={`image-banner-2`} className="relative w-full lg:h-[670px] md:h-[550px] h-[450px] ">
             <Image
-              src={'/images/banner/airbnb-home.jfif'}
+              src={'/images/banner/airbnb-banner.jpg'}
 
               alt={`Banner 4`} className="w-full h-full "
               layout="fill"
