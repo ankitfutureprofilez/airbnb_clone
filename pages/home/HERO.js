119ea3ca0bceb9a13682269da7cba233250e1fc9
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-06-16 17:56:48 +0530

    airbnb image

--- pages/home/HERO.js
+++ pages/home/HERO.js
@@ -24,7 +24,7 @@ export default function HERO() {
         <SwiperSlide>
           <div key={`image-banner-4`} className="relative w-full lg:h-[670px] md:h-[550px] h-[450px]">
             <Image
-              src={'/images/banner/airbnb-home.jfif'}
+              src={'/images/banner/Banner33.jpg'}
 
               alt={`Banner 4`} className="w-full h-full "
               layout="fill"
@@ -34,7 +34,7 @@ export default function HERO() {
         <SwiperSlide>
           <div key={`image-banner-1`} className="relative w-full lg:h-[670px] md:h-[550px] h-[450px]">
             <Image
-              src={'/images/banner/Banner7.jpg'}
+              src={'/images/banner/banner7.jpg'}
 
               alt={`Banner 4`} className="w-full h-full object-cover"
               layout="fill"
@@ -55,7 +55,7 @@ export default function HERO() {
         <SwiperSlide>
           <div key={`image-banner-3`} className="relative w-full lg:h-[670px] md:h-[550px] h-[450px]">
             <Image
-              src={'/images/banner/Banner7.jpg'}
+              src={'/images/banner/airbnb-home.jfif'}
               alt={`Banner 4`} className="w-full h-full"
               layout="fill"
               objectFit="cover" />
